ebbc95a23bf54e98cce538cd7162f30dbadca595
Rhythem Sharma <Rid.Sharma@utexas.edu>
2017-12-11 11:08:18 -0600

    Completing the final project

--- main.m
+++ main.m
@@ -1,9 +1,11 @@
+
 %Runs the scripts that plots the data into multiple images 
 for n = 10:2:20
     run(['GraphAt' num2str(n) 'Days.m']);
 end
 
 %Runs the script that plots the graph with error bars
+run('errorInCellCount.m');
 
 %Runs the script that plots the Gompertzian fit along side the observed graph
 run('mathematicalModel.m');
